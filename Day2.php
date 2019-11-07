f0ea5d029499f49b37d8917cd07b27f18e6ec7c8
jeet1709 <45985669+jeet1709@users.noreply.github.com>
2019-11-07 21:27:15 +0530

    Add files via upload

---
+++ Day2.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+   <b> Welcome</b> <?php echo $_POST["fname"];" ";echo $_POST["lname"] ?><br>
+	<b>Address:</b><?php echo $_POST["add"]; ?><br>
+	<b>Roll number:</b><?php echo $_POST["roll"]; ?><br>
+	<b>Semester:</b><?php echo $_POST["sem"]; ?><br>
+	<b>Father's name:</b><?php echo $_POST["faname"]; ?><br>
+	<b>Email id:</b> <?php echo $_POST["email"]; ?><br>
+   <b> Mobile No.:</b><?php echo $_POST["phone"]; ?><br>
+</body>    
+</html>
